93428d7df770c33fdc700800eb0e35655f0deab4
fanxuewen <1020977852@qq.com>
2019-03-22 21:52:14 +0800

    change pictures

--- web/src/components/home/PicturesShare.vue
+++ web/src/components/home/PicturesShare.vue
@@ -7,15 +7,19 @@
         :class="['scroller-item',item.id==selectedId? 'scroller-active':'']"
         v-for="item in pictures"
         :key="item.id"
-        @click="selectedId=item.id"
+        @click="getPictursById(item.id)"
       >{{item.title}}</a>
     </div>
 
-    <ul>
-  <li v-for="item in list" :key="item.id">
-    <img v-lazy="item">
-  </li>
-</ul>
+    <ul class="picture-list">
+      <li v-for="item in list" :key="item.imgUrl" class="pictur-item">
+        <img v-lazy="item.imgUrl">
+        <div class="info">
+            <h4 class="info-title">{{item.title}}</h4>
+            <div class="info-body">{{item.brife}}</div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -27,21 +31,39 @@ export default {
   data() {
     return {
       pictures: [{ id: 0, title: "全部" }],
-      selectedId: 0
+      selectedId: 0,
+      list: []
     };
   },
   created() {
-    this.getPictursHeader();
+    this.getPicturs();
   },
 
   methods: {
-    getPictursHeader() {
+    getPicturs() {
       let that = this;
       axios
         .get("api/pictures")
         .then(function(response) {
-            console.log(response);
-          that.pictures = that.pictures.concat(response.data.result);
+          that.pictures = that.pictures.concat(response.data[0]);
+          that.list = response.data[1];
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    getPictursById(id) {
+      this.selectedId = id;
+      let that = this;
+      axios
+        .get("api/pictures/type", {
+          params: {
+            id,
+            time: new Date().getTime()
+          }
+        })
+        .then(function(response) {
+          that.list = response.data.reverse();
         })
         .catch(function(error) {
           console.log(error);
@@ -52,6 +74,9 @@ export default {
 </script>
 
 <style lang='scss' scoped>
+* {
+  box-sizing: border-box;
+}
 .scroller {
   width: 100%;
   height: 3rem;
@@ -79,9 +104,59 @@ export default {
 .scroller::-webkit-scrollbar {
   display: none;
 }
-img[lazy=loading] {
-  width: 40px;
-  height: 300px;
-  margin: auto;
+.picture-list {
+  padding-top: 1rem;
+  list-style: none;
+  background-color: #e4dddd;
+  li {
+    text-align: center;
+    padding: 0.2rem;
+    display: inline-block;
+    width: 98%;
+    box-shadow: 0 0 0.2rem #c5b9b9;
+    position: relative;
+    .info{
+        color: white;
+        text-align: left;
+        position: absolute;
+        bottom: 0;
+      
+        width: 100%;
+        background: rgba(213, 207, 207, 0.8);
+        .info-title{
+          margin-bottom: .5rem;
+        }
+        .info-body{
+              font-size: 0.8rem;
+        }
+    }
+   
+    img,
+    img[lazy="loading"] {
+      width: 100%;
+      height: 25rem;
+        
+    }
+  }
+  li:hover{
+     box-shadow: 0 0 0.2rem #774545;; 
+  }
+  @media (min-width: 576px) {
+    li {
+      width: 49%;
+    }
+  }
+
+  @media (min-width: 992px) {
+    li {
+      width: 33.3%;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    li {
+      width: 25%;
+    }
+  }
 }
 </style>
--- web/src/main.js
+++ web/src/main.js
@@ -1,20 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import { Swipe, SwipeItem,Lazyload } from 'mint-ui';
+import MintUI from 'mint-ui';
 import moment from 'moment'
-
-
-Vue.use(Lazyload);
- 
-
-
-
-Vue.component(Swipe.name, Swipe);
-Vue.component(SwipeItem.name, SwipeItem);
+Vue.use(MintUI);
 Vue.use(VueRouter);
 Vue.config.productionTip = false
-
 Vue.filter('dateFormat',function(date,pattern="YYYY-MM-DD HH:mm:ss"){
    
      return moment(date).format(pattern);
@@ -22,8 +13,6 @@ Vue.filter('dateFormat',function(date,pattern="YYYY-MM-DD HH:mm:ss"){
 
 import router from './router'
 import '../node_modules/mint-ui/lib/swipe/style.css'
-
-
 
 new Vue({
   el:'#app',
